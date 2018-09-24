e76e90964efa71cade085a5bde8a6da07c0bf1c0
taylus <therealbjohnson@gmail.com>
2018-09-23 20:38:58 -0500

    Add project files.

---
+++ GBPrng/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GBPrng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
