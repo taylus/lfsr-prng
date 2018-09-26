78e7f43ab164d9ae341225d36a1ad2e1fc8ca427
taylus <therealbjohnson@gmail.com>
2018-09-25 21:57:53 -0500

    Refactoring

--- GBPrng/Program.cs
+++ GBPrng/Program.cs
@@ -1,38 +1,59 @@
 ﻿using System;
 using System.Linq;
-using System.Diagnostics;
 
 namespace GBPrng
 {
     public class Program
     {
-        private const int Iterations = 1000000;
-
         public static void Main()
         {
-            //ushort seed = 0xbabe;
-            var seed = (ushort)DateTime.Now.Ticks;
+            ushort seed = 0xbabe;
+            //var seed = (ushort)DateTime.Now.Ticks;
+
+            PrintRun(seed, iterations: 10);
+            //PrintAnalysis(seed, iterations: 1_000_000);
+        }
+
+        /// <summary>
+        /// Prints the given # of random numbers generated using the given seed.
+        /// </summary>
+        private static void PrintRun(ushort seed, int iterations)
+        {
+            var rng = new LinearFeedbackShiftRegisterPrng(seed);
+
+            Console.WriteLine($"Seed: {seed:x4}");
+            for (int i = 0; i < iterations; i++)
+            {
+                byte result = rng.Generate();
+                Console.WriteLine($"Random number: {result:x2}");
+            }
+        }
+
+        /// <summary>
+        /// Prints the distribution of generating the given number of random
+        /// numbers using the given seed.
+        /// </summary>
+        private static void PrintAnalysis(ushort seed, int iterations)
+        {
             var rng = new LinearFeedbackShiftRegisterPrng(seed);
 
             var resultCounts = Enumerable.Range(0, 256)
                 .Select(i => new { Key = (byte)i, Value = 0 })
                 .ToDictionary(k => k.Key, v => v.Value);
 
-            for (int i = 0; i < Iterations; i++)
+            Console.WriteLine($"Seed: {seed:x4}");
+            for (int i = 0; i < iterations; i++)
             {
                 byte result = rng.Generate();
                 resultCounts[result]++;
-
-                //print the first few iterations for comparing runs w/ same seed
-                if (i < 10) Debug.WriteLine(result);
             }
 
-            Console.WriteLine($"Distribution of random numbers generated over {Iterations:n0} iterations:{Environment.NewLine}");
-            Console.WriteLine("  # | Occurrences");
-            Console.WriteLine("----+------------");
+            Console.WriteLine($"Distribution of random numbers generated over {iterations:n0} iterations:{Environment.NewLine}");
+            Console.WriteLine(" # | Occurrences");
+            Console.WriteLine("---+------------");
             foreach (var kvp in resultCounts)
             {
-                Console.WriteLine($"{kvp.Key,3} | {kvp.Value:n0}");
+                Console.WriteLine($"{kvp.Key:x2} | {kvp.Value:n0}");
             }
         }
     }
